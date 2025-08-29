59bf1f208b4bd421963610b4175b8a82565278f4
shoieb-alam <shoieb.ctg@gmail.com>
2025-08-29 20:27:01 +0600

    add verifyToken in make-admin route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { checkIfAdmin, addUser, upsertUser, setAdminRole } = require("../controllers/userController");
+const verifyToken = require("../middlewares/verifyToken");
 
 router.get("/:email", checkIfAdmin);
 router.post("/", addUser);
 router.put("/", upsertUser);
-router.put("/admin", setAdminRole);
+router.put("/make-admin", verifyToken, setAdminRole);
 
 module.exports = router;
