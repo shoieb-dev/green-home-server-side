860af0048b80cd97d797bf7d36630c759dec167d
shoieb-alam <shoieb.ctg@gmail.com>
2025-09-02 08:22:03 +0600

    add updateHouse route to houseRoutes

--- routes/houseRoutes.js
+++ routes/houseRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllHouses, getHouseById, createHouse, deleteHouse } = require("../controllers/houseController");
+const { getAllHouses, getHouseById, createHouse, updateHouse, deleteHouse } = require("../controllers/houseController");
 
 router.get("/", getAllHouses);
 router.get("/:id", getHouseById);
 router.post("/", createHouse);
+router.put("/:id", updateHouse);
 router.delete("/:id", deleteHouse);
 
 module.exports = router;
